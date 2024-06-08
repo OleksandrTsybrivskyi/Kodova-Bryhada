682910384b5a1dcc817413839acb65cc3fbfd81e
Daryna <darynachepil@gmail.com>
2024-06-08 16:16:14 +0300

    add subjects

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ class Account(db.Model):
     email = db.Column(db.String(30), nullable=False, unique=True)
     role = db.Column(db.String(30), nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Account %r>' % self.id
@@ -23,13 +22,28 @@ class Account(db.Model):
 class Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subject_name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(30), nullable=False, unique=True)
-    role = db.Column(db.String(30), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    teacher_id = db.Column(db.Integer)
 
     def __repr__(self):
         return '<Subject %r>' % self.id
+
+class Mark(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    SS_id = db.Column(db.Integer)
+    value = db.Column(db.Integer)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Mark %r>' % self.id
+
+class StudentSubject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+    subject_name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return '<StudentSubject %r>' % self.id
 
 with app.app_context():
     account = Account(email="Admin@gmail.com", full_name="Admin", password="Admin", role="Admin")
@@ -66,17 +80,52 @@ def login():
     return render_template("login.html", message=False)
 
 
-@app.route("/account/<int:id>")
+@app.route("/account/<int:id>",methods=['POST', 'GET'])
 def your_account(id):
     account = Account.query.get(id)
     return render_template("your_account.html", account=account)
+
+@app.route("/greate_subgect/<int:id>", methods=['POST', 'GET'])
+def greate_subgect(id):
+    account = Account.query.get(id)
+    print(account.role)
+    if account.role == "вчитиль":
+        if request.method == "POST":
+            subject_name = request.form["subject_name"]
+            subject = Subject(subject_name=subject_name, teacher_id=id)
+
+            try:
+                db.session.add(subject)
+                db.session.commit()
+            except:
+                return render_template("greate_subgect.html", message="error", account=account)
+        try:
+            subjects = Subject.query.filter_by(teacher_id=id).order_by(Subject.subject_name).all()
+            return render_template("greate_subgect.html", account=account, subjects=subjects)
+        except:
+            return render_template("greate_subgect.html", account=account)
+    elif account.role == "учень":
+        if request.method == "POST":
+
+            subject_id = request.form["subject_name"]
+            subject_name = Subject.query.get(subject_id).subject_name
+            ss = StudentSubject(subject_id=subject_id, user_id=id, subject_name=subject_name)
+            db.session.add(ss)
+            db.session.commit()
+            sss = StudentSubject.query.filter_by(user_id=id).all()
+            return render_template("greate_subgect.html", account=account, sss=sss)
+
+    return render_template("greate_subgect.html", account=account) # Додайте цей рядок
+
+
 
 
 @app.route("/sign_up", methods=['POST', 'GET'])
 def sign_up():
     if request.method == "POST":
         email = request.form["email"]
-        role = "Учень" if request.form.get("role") == "on" else "вчитиль"
+        print(request.form.get("role"))
+        role = "учень" if request.form.get("role") == "student" else "вчитиль"
         full_name = request.form["full_name"]
         password = request.form["password"]
         password2 = request.form["password2"]
@@ -84,18 +133,19 @@ def sign_up():
             return render_template("sign_up.html", message="Введіть два однакові паролі")
 
         # Створення нового об'єкта Account
-        account = Account(email=email, full_name=full_name, password=password, role=role)
+        with app.app_context():
+            account = Account(email=email, full_name=full_name, password=password, role=role)
 
-        try:
-            # Додавання об'єкта до сесії
-            db.session.add(account)
-            # Збереження змін до бази даних
-            db.session.commit()
-            return redirect(f"/account/{account.id}")
+            try:
+                # Додавання об'єкта до сесії
+                db.session.add(account)
+                # Збереження змін до бази даних
+                db.session.commit()
+                return redirect(f"/account/{account.id}")
 
-        except IntegrityError:
-            # Обробка випадку, коли обліковий запис з такою електронною адресою вже існує
-            return render_template("sign_up.html", message="Цей обліковий запис вже існує")
+            except IntegrityError:
+                # Обробка випадку, коли обліковий запис з такою електронною адресою вже існує
+                return render_template("sign_up.html", message="Цей обліковий запис вже існує")
 
     else:
         return render_template("sign_up.html")
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
